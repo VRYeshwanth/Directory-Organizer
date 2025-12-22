d2d2697fda91dcf21b303c148096885ef273e539
Yeshwanth <vryeshwanth06@gmail.com>
2025-12-22 16:23:24 +0530

    Basic App Layout

---
+++ main.js
@@ -0,0 +1,24 @@
+import { app, BrowserWindow, ipcMain } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 600,
+        height: 600,
+        resizable: false,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true,
+            nodeIntegration: false,
+            sandbox: false,
+        },
+    });
+
+    mainWindow.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
